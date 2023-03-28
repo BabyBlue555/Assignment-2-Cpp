1802defd6ff71ed34c860f7d6faef402a7b299db
michal <michalruth144@gmail.com>
2023-03-28 16:00:34 +0300

    add test cases

--- Test.cpp
+++ Test.cpp
@@ -19,7 +19,8 @@ TEST_CASE("Test 1 - check stacksize and cardesTaken ") {
     CHECK(p2.stacksize() == 26);
     CHECK(p1.cardesTaken() == 0);
     CHECK(p2.cardesTaken() == 0);
-    game.playTurn();
+    CHECK_NOTHROW(game.playTurn());
+    //game.playTurn();
     // after playing one turn:
     CHECK((p1.cardesTaken()==0 ||p1.cardesTaken()==2 || p1.cardesTaken()==6)); // lost,won or draw and won
     CHECK((p2.cardesTaken()==0 ||p2.cardesTaken()==2 || p1.cardesTaken()==6)); // lost,won or draw and won
@@ -32,7 +33,9 @@ TEST_CASE("Test 1 - check stacksize and cardesTaken ") {
 }
 
 TEST_CASE("check Player constructor"){
-    Player p1("Alice");
+    CHECK_NOTHROW(Player p1("Alice")); // okay
+   // Player p1("Alice");
+    CHECK_THROWS(Player("")); // shouln't create a player without a name
     CHECK_THROWS(Player("Alice")); // can't create another player with the same name
 }
 
@@ -40,12 +43,16 @@ TEST_CASE("check Game constructor"){
 
     Player p1("Alice");
     Player p2("Bob");
+    Player p3("Shimi");
 
-    CHECK_NOTHROW(Game(p1,p2)); // create a new game - shouldn't be any errors
+    CHECK_NOTHROW(Game game(p1,p2)); // create a new game - shouldn't be any errors
+    //Game game(p1,p2);
+    CHECK_THROWS(Game(p1,p2)); // can't create another game with same players that currently play another game
 
-    CHECK_THROWS(Game(p1,p2)); // can't create another game with same players
+    CHECK_THROWS(Game(p1,p3)); // can't create another game when one of the players currently plays in another game 
 
-    CHECK_THROWS(Game(p1,p1));
+    CHECK_THROWS(Game (p1,p1)); // can't create game with same player
+
 
     //game.playAll();
 
@@ -55,12 +62,11 @@ TEST_CASE("check print functions when game didn't start "){
     Player p1("Alice");
     Player p2("Bob");
     Game game(p1,p2); 
-    //CHECK_THROWS(game.playTurn());
     // no turn was played - the game didn't start - throw exceptions
-    CHECK_THROWS(game.printLastTurn());
-    CHECK_THROWS(game.printStats());
-    CHECK_THROWS(game.printLog());
-    CHECK_THROWS(game.printWiner());
+    CHECK_THROWS(game.printLastTurn()); 
+    CHECK_THROWS(game.printStats()); // there are no stats until the first turn!
+    CHECK_THROWS(game.printLog()); //there is nothing to print here until the first turn!
+    CHECK_THROWS(game.printWiner());// there is no winner at that point!
 
 }
 
@@ -70,11 +76,7 @@ TEST_CASE("check printTurn function when game is over "){
     Player p2("Bob");
     Game game(p1,p2); 
     game.playAll();
-  
-
     CHECK_THROWS(game.playTurn());// the game is over, can't do another turn
-    
-
 }
 
 
@@ -82,10 +84,8 @@ TEST_CASE("check print functions after game starts"){
     Player p1("Alice");
     Player p2("Bob");
     Game game(p1,p2);
-    // CHECK_THROWS(game.printStats());// there are no stats until the first turn!
-    // CHECK_THROWS(game.printLog());//there is nothing to print here until the first turn!
-    // CHECK_THROWS(game.printWiner()); // there is no winner at that point!
     CHECK_NOTHROW(game.playTurn()); 
+    // game.playTurn();
     CHECK_NOTHROW(game.printLastTurn()); 
     CHECK_NOTHROW(game.printStats()); // after first turn there are stats
     CHECK_NOTHROW(game.printLog()); //  after first turn there is log
@@ -94,10 +94,27 @@ TEST_CASE("check print functions after game starts"){
         CHECK_NOTHROW( game.playTurn());
     }
     CHECK_THROWS(game.printWiner()); // the game isn't over yet
+    // game.playAll();
+    // CHECK_NOTHROW(game.printWiner()); // one of the players won
+
+}
+
+
+// end game has two cases - one of them wins / tie 
+TEST_CASE("one of the players wins"){
+    Player p1("Alice");
+    Player p2("Bob");
+    Game game(p1,p2);
     game.playAll();
-    CHECK_NOTHROW(game.printWiner());
+    CHECK_NOTHROW(game.printWiner()); // one of the players won
 
+}
 
-
-    // here i can do more   CHECK_NOTHROW
+// i did this case based on the answer i got in the forum.
+TEST_CASE("tie between the players"){
+    Player p1("Alice");
+    Player p2("Bob");
+    Game game(p1,p2);
+    game.playAll();
+    CHECK_THROWS(game.printWiner()); // non of the players won - its a tie
 }
